03a59888e524575c69927abec332efb4e815cf6a
MyoMyintAung <myomyintaung064@gmail.com>
2022-04-17 18:38:06 +0630

    added hex color flipper & styling

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,6 +1,6 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const bgText = document.querySelector(".bg-text");
-const btn = document.querySelector(".click-me");
+const bgText = document.querySelector(".color");
+const btn = document.querySelector(".btn-hero");
 
 const getRandomNumber = () => {
   const randomNum = Math.floor(Math.random() * colors.length);
@@ -11,7 +11,7 @@ const changeBg = () => {
   const rand = getRandomNumber();
   const color = colors[rand];
 
-  bgText.innerHTML = color;
+  bgText.textContent = color;
   document.body.style.backgroundColor = color;
 };
 
--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,1 +1,27 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const bgText = document.querySelector(".color");
+const btn = document.querySelector(".btn-hero");
+
+const getRandomNumber = () => {
+  const randomNum = Math.floor(Math.random() * hex.length);
+  return randomNum;
+};
+
+const getHexCode = () => {
+  let hexColor = "#";
+  for (let i = 0; i <= 5; i++) {
+    const randNum = getRandomNumber();
+    hexColor += hex[randNum];
+  }
+  return hexColor;
+};
+
+const changeBg = () => {
+  const color = getHexCode();
+
+  bgText.textContent = color;
+  document.body.style.backgroundColor = color;
+};
+
+btn.addEventListener("click", changeBg);
