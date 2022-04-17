d33fdc0879a8c5f0fcaabf15373bf280e8e21af6
MyoMyintAung <myomyintaung064@gmail.com>
2022-04-17 16:26:52 +0630

    simple flipper

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const bgText = document.querySelector(".bg-text");
+const btn = document.querySelector(".click-me");
+
+const getRandomNumber = () => {
+  const randomNum = Math.floor(Math.random() * colors.length);
+  return randomNum;
+};
+
+const changeBg = () => {
+  const rand = getRandomNumber();
+  const color = colors[rand];
+
+  bgText.innerHTML = color;
+  document.body.style.backgroundColor = color;
+};
+
+btn.addEventListener("click", changeBg);
